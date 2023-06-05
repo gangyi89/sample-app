cac8d75dc72647a665c2f51e156b3f4154830e24
gangyi89@gmail.com <gangyi89@gmail.com>
2023-06-05 18:03:18 +0800

    update code to trigger post request

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,38 +1,21 @@
 import React from "react";
 import axios from "axios";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const Main = () => {
   const [fqdn, setFqdn] = useState("");
   const [error, setError] = useState("");
+  const [token, setToken] = useState("");
+
 
   console.log("fqdn: ", fqdn);
 
   const handleChange = (event) => {
-    setFqdn(event.target.value);
+    setToken(event.target.value);
   };
 
-  const postFqdn = () => {
-    const config = {
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
-        // "Access-Control-Allow-Origin": "*",
-      },
-    };
-    const data = "token=4370dcbc-940f-6961-43a9-7129dbea0018";
-    axios
-      .post(fqdn, data, config)
-      .then((respond) => {
-        //do-something
-        console.log("respond", respond);
-      })
-      .catch((error) => {
-        console.log(error);
-        console.log("error", error.message);
-        setError(error.message);
-      });
-  };
+
   return (
     <div className="text-black">
       <div className="max-w-[800p] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center">
@@ -46,23 +29,21 @@ const Main = () => {
           </p>
         </div>
         <p className="md:text-xl text-xl text-gray-600">
-          Input FQDN and click on submit to trigger a axios request
+          Input token and submit to ePay
         </p>
         <div>
           <input
             type="text"
             id="large-input"
             onChange={handleChange}
-            value={fqdn}
+            value={token}
             className="w-[550px] p-4 mt-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-md focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
           />
         </div>
-        <button
-          onClick={postFqdn}
-          className="bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto py-3 text-black"
-        >
-          Submit
-        </button>
+        <form method="post" action="https://esepaydemo-neit5c7efq-an.a.run.app/select">
+          <input type="hidden" value={token} name="token" id="token" />
+          <button className="bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto py-3 text-black">Proceed to ePay</button>
+        </form>
         <p className="text-rose-700">{error}</p>
       </div>
     </div>
